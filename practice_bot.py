b4860eb7b76d41156c65f9aeb0f58a6ba694aca5
Hagop Alajajian <114875390+hagop-a@users.noreply.github.com>
2023-10-20 04:22:44 -0400

    Add files via upload

---
+++ practice_bot.py
@@ -0,0 +1,23 @@
+from nltk import Chat, reflections
+
+# Define the chat pairs and patterns.
+pairs = [
+    [r'my name is (.*)', ['hi %1']],
+    [r'hello', ['Hello! How can I assist you?']],
+    [r'hi', ['Hi there! How can I help you today?']],
+    [r'how are you', ['I\'m just a program, so I don\'t have feelings, but thanks for asking!']],
+    [r'you are amazing', ['Thank you! How can I assist you today?']],
+    [r'quit', ['Goodbye! If you have more questions, just ask.']]
+]
+
+# Create the chatbot.
+chat = Chat(pairs, reflections)
+
+# This function will start the conversation.
+def start_chat():
+    print("Hello! Type \"quit\" to exit.")
+    chat.converse()
+
+# Run the chatbot function.
+if __name__ == "__main__":
+    start_chat()
